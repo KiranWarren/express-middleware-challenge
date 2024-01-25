559e41e7c9e7fecc0088f0b85e5c31ba111dfed1
Kiran Warren <kiran.m.warren@gmail.com>
2024-01-25 16:29:57 +1000

    solution

--- index.js
+++ index.js
@@ -7,16 +7,40 @@ app.use(express.urlencoded({ extended: true }));
 
 const baseURL = "https://pokeapi.co/api/v2/pokemon/";
 
-// localhost:3000
-app.post("/", async (request, response) => {
-  // If this fetch request return JSON, then we want to store it as an object that we can work with.
-  let result = await fetch(baseURL + request.body.pokemonName).then((data) => {
-    return data.json();
+// Middleware function, check if valid response is received from the API
+// Return json if valid response, return error if invalid API response
+const isPokemonValid = async (request, response, next) => {
+  // Retrieve response from API
+  await fetch(baseURL + request.body.pokemonName).then(async (data) => {
+    // Convert data from API to string and store in variable
+    let dataText = await data.text();
+    // Check if the data string is an error from the API. Return an error if so, otherwise return the response
+    if (dataText == "Not Found") {
+      next(new Error("Pokemon not found."));
+    } else {
+      request.body.pokeApiResult = JSON.parse(dataText);
+      next();
+    }
   });
+};
 
+// Middleware function, handles a possible error from the previous middleware
+const handleInvalid = (error, request, response, next) => {
+  if (error) {
+    console.log(error.message);
+    response.status(400).json({
+      error: error.message,
+    });
+  }
+};
+
+// localhost:3000
+app.post("/", isPokemonValid, handleInvalid, async (request, response) => {
+  // Middleware has already retrieved the response.
+  // If the code has reached this point, then the data must be valid. (I.e. handleInvalid did not trigger a response.)
   response.json({
-    pokedexNumber: result.id,
-    name: result.name,
+    pokedexNumber: request.body.pokeApiResult.id,
+    name: request.body.pokeApiResult.name,
   });
 });
 
